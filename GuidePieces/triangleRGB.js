f50a9623952eda279da10c884d06f7cdfb47b6c0
SenonLi <senonli6510@gmail.com>
2016-11-08 13:51:17 -0500

    Check webgl support for IE / Google Chrome/ FireFox

--- GuidePieces/triangleRGB.js
+++ GuidePieces/triangleRGB.js
@@ -1,3 +1,17 @@
 var onLoadShow = function ()    {
     console.log('This is working');
+     
+    var canvas = document.getElementById('gameSurface');
+    
+    var gl = canvas.getContext('webgl');
+    
+    if(!gl) {
+        console.log('WebGL is not supported');
+        gl = canvas.getContext('experimental-webgl');
+    }
+    
+    if(!gl) {
+        alert('Your browser does not support WebGL');
+    }
+    
 }
