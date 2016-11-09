5f18a21a32c11baf23cb712c89058cc2b53d7f3c
SenonLi <senonli6510@gmail.com>
2016-11-08 22:01:04 -0500

    self-Rotating triangleRGB

--- JavaScriptPieces/triangleRGB.js
+++ JavaScriptPieces/triangleRGB.js
@@ -1,5 +1,4 @@
-var vertexShaderTest = 
-[
+var vertexShaderTest = [
     'precision mediump float;',
     '',
     'attribute vec3 vertPosition;',
@@ -13,13 +12,12 @@ var vertexShaderTest =
     '',
     'void main()',
     '{',
-    '   gl_Position = projection * model * view * vec4(vertPosition, 1.0);',
+    '   gl_Position = projection * view * model *  vec4(vertPosition, 1.0);',
     '   toFragColor = vertColor;',
-    '}'    
+    '}'
 ].join('\n');
 
-var fragmentShaderText =
-[
+var fragmentShaderText = [
     'precision mediump float;',
     '',
     'varying vec3 toFragColor;',
@@ -27,134 +25,144 @@ var fragmentShaderText =
     'void main()',
     '{',
     '   gl_FragColor = vec4(toFragColor, 1.0);',
-    '}' 
+    '}'
 ].join('\n');
 
-var onLoadShow = function ()    {
+var onLoadShow = function () {
     console.log('This is working');
-     
+
     var canvas = document.getElementById('gameSurface');
-    
+
     var gl = canvas.getContext('webgl');
-    
+
     // for IE web browser, need to go for 'experimental-webgl'
-    if(!gl) {
+    if (!gl) {
         console.log('WebGL not supported \n falling back on experimental-webgl');
         gl = canvas.getContext('experimental-webgl');
     }
-    
-    if(!gl) {
+
+    if (!gl) {
         alert('Your browser does not support WebGL');
     }
-    
+
     // Adjust the canvas based on screen size
     {
         canvas.width = 0.9 * window.innerWidth;
         canvas.height = 0.7 * window.innerHeight;
         gl.viewport(0, 0, canvas.width, canvas.height);
     }
-    
+
     gl.clearColor(0.75, 0.85, 0.8, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);   
-    
+
     //
     // Create shaders
     //
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    
+
     gl.shaderSource(vertexShader, vertexShaderTest);
     gl.shaderSource(fragmentShader, fragmentShaderText);
-    
+
     gl.compileShader(vertexShader);
-    if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
         console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
         return;
     }
     gl.compileShader(fragmentShader);
-     if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
         console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
         return;
     }
-    
+
     var program = gl.createProgram();
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
     gl.linkProgram(program);
-    if(!gl.getProgramParameter(program, gl.LINK_STATUS))    {
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
         console.error('ERROR linking program!', gl.getProgramInfoLog(program));
         return;
     }
-    
+
     gl.validateProgram(program);
-    if(!gl.getProgramParameter(program, gl.VALIDATE_STATUS))    {
+    if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
         console.error('ERROR validating program!', gl.getProgramInfoLog(program));
         return;
     }
-    
+
     //
     // Create buffer
     //
-    var triangleVertices =
-    [  //X,   Y             R,  G,  B
-        0.0, 0.5, -0.0,      1.0, 0.0, 0.0,
-        -0.5, -0.5, -0.0,    0.0, 1.0, 0.0,
-        0.5, -0.5, -0.0,     0.0, 0.0, 1.0
+    var triangleVertices = [ //X,   Y             R,  G,  B
+        0.0, 0.5, -0.0, 1.0, 0.0, 0.0,
+        -0.5, -0.5, -0.0, 0.0, 1.0, 0.0,
+        0.5, -0.5, -0.0, 0.0, 0.0, 1.0
     ];
-    
+
     var triangleVBO = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, triangleVBO);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
-    
+
     var verPositionLocation = gl.getAttribLocation(program, 'vertPosition');
     var verColorLocation = gl.getAttribLocation(program, 'vertColor');
-    
+
     gl.vertexAttribPointer(
         verPositionLocation, // Attribute location
-        3,  // Number of elements per attribute
+        3, // Number of elements per attribute
         gl.FLOAT, // Type of elements
         gl.FALSE, //
-        6 * Float32Array.BYTES_PER_ELEMENT,// Size of an indivisual vertex
-        0// Offset from the beginning of a single vertex to this attribute
+        6 * Float32Array.BYTES_PER_ELEMENT, // Size of an indivisual vertex
+        0 // Offset from the beginning of a single vertex to this attribute
     )
     gl.enableVertexAttribArray(verPositionLocation);
-    
+
     gl.vertexAttribPointer(verColorLocation, 3, gl.FLOAT,
-        gl.FALSE,  
+        gl.FALSE,
         6 * Float32Array.BYTES_PER_ELEMENT,
         3 * Float32Array.BYTES_PER_ELEMENT
     )
     gl.enableVertexAttribArray(verColorLocation);
-    
-   
-    gl.useProgram(program);
+
 
     var model = new Float32Array(16);
     var view = new Float32Array(16);
     var projection = new Float32Array(16);
-
-    mat4.identity(model);
+    var identity = new Float32Array(16);
+    mat4.identity(identity);
     mat4.lookAt(view, [0.0, 0.0, 2.0], [0.0, 0.0, 0.0], [0.0, 1.0, 0.0]);
     mat4.perspective(projection, glMatrix.toRadian(45.0), canvas.width / canvas.height, 0.1, 100);
 
-    gl.uniformMatrix4fv(gl.getUniformLocation(program, 'model'), gl.FALSE, model);   
-    gl.uniformMatrix4fv(gl.getUniformLocation(program, 'view'), gl.FALSE, view);  
-    gl.uniformMatrix4fv(gl.getUniformLocation(program, 'projection'), gl.FALSE, projection);
     //
     // Main render loop
     //
     // Commen JavaScript Animation
-///*    var loop = function() {
-//        updateWorld();
-//        renderWorld();
-//        if (running)    {
-//            requestAnimationFrame(loop);
-//        }
-//    }
-//    requestAnimationFrame(loop);*/
-    
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
-        
+    ///*    var loop = function() {
+    //        updateWorld();
+    //        renderWorld();
+    //        if (running)    {
+    //            requestAnimationFrame(loop);
+    //        }
+    //    } */
+    var angle = 0.0;
+    var paintloop = function () {
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        angle = performance.now() / 1000.0 / 6.0 * 2.0 * Math.PI;
+        mat4.rotate(model, identity, angle, [0.0, 1.0, 0.0]);
+
+        gl.useProgram(program);
+
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, 'model'), gl.FALSE, model);
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, 'view'), gl.FALSE, view);
+        gl.uniformMatrix4fv(gl.getUniformLocation(program, 'projection'), gl.FALSE, projection);
+
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+
+        requestAnimationFrame(paintloop);
+    }
+    requestAnimationFrame(paintloop);
+
+
 }
 
 
@@ -165,24 +173,3 @@ var onLoadShow = function ()    {
 //        gl_Position: [vertPosition.x, vertPosition.y, 0.0, 1.0]
 //    };
 //}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Notes.md
+++ Notes.md
@@ -6,3 +6,8 @@
 * If putting the js inside <head> tag, there won't be any guarantee which one is called first.
 * Everything is 64bit floating point precision, whereas opengl expects 32bit
 
+## OpenGL / WebGL
+* Remember, inside the vertexShader, the 3D paint math has to be model -> view -> projection from going from right to the left.
+* above means:     gl_Position = projection * view * model * vec4(position, 1.0f);
+
+
